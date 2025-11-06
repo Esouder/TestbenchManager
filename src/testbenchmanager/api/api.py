cb7ece0d09b90748db3e64fb0ff8cac138cdca75
Eric <eric@souder.ca>
2025-11-06 01:02:02 -0800

    CORS shit

--- src/testbenchmanager/api/api.py
+++ src/testbenchmanager/api/api.py
@@ -1,9 +1,23 @@
 """API root"""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import instrument_router
 
 api = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
 
 api.include_router(instrument_router)
